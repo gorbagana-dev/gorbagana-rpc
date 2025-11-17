c7c512a7b2ad1fee676b44f0c8c70f8246ab6b86
Parvat Raj Singh <parvat.raj2@gmail.com>
2025-11-17 16:42:35 +0530

    Added build for linux

--- stack-orchestrator/container-build/gorbagana-gorchain-base/build.sh
+++ stack-orchestrator/container-build/gorbagana-gorchain-base/build.sh
@@ -9,7 +9,7 @@ if [ ! -d "$GORCHAIN_REPO" ]; then
   exit 1
 fi
 
-docker build -t gorbagana/gorchain-base:local \
+docker build --platform linux/amd64 -t gorbagana/gorchain-base:local \
   -f ${SCRIPT_DIR}/Dockerfile \
   ${build_command_args} \
   ${GORCHAIN_REPO}
--- stack-orchestrator/container-build/gorbagana-gorchain-restarter/build.sh
+++ stack-orchestrator/container-build/gorbagana-gorchain-restarter/build.sh
@@ -3,7 +3,7 @@ source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker build -t gorbagana/gorchain-restarter:local \
+docker build --platform linux/amd64 -t gorbagana/gorchain-restarter:local \
   -f ${SCRIPT_DIR}/Dockerfile \
   ${build_command_args} \
   ${SCRIPT_DIR}
